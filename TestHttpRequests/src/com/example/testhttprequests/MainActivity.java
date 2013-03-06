a7fa6a64edbef5b2b97a4caabcd598409c44c70a
Matt Spitz <mattspitz@gmail.com>
2013-03-06 16:59:08 -0500

    Adding example for persistent cookie storage with login info.

--- TestHttpRequests/src/com/example/testhttprequests/MainActivity.java
+++ TestHttpRequests/src/com/example/testhttprequests/MainActivity.java
@@ -1,10 +1,21 @@
 package com.example.testhttprequests;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.PersistentCookieStore;
+import com.loopj.android.http.RequestParams;
 
 public class MainActivity extends Activity {
+
+	private static final String TAG = "MainActivity";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,4 +30,106 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	private AsyncHttpClient asyncClient = null;
+	private static final String BASE_URL = "http://10.0.2.2:5000/v1/"; // localhost!
+	private AsyncHttpClient getAsyncHttpClient() {
+		if (asyncClient == null) {
+			asyncClient = new AsyncHttpClient();
+			asyncClient.setCookieStore(new PersistentCookieStore(getApplication()));
+		}
+		return asyncClient;
+	}
+
+	private String getTextFieldValue(int viewId) {
+		return ((TextView)findViewById(viewId)).getText().toString();
+	}
+
+	public void onCreateClick(View view) {		
+		RequestParams params = new RequestParams();
+		params.put("realname", getTextFieldValue(R.id.create_person));
+		params.put("username", getTextFieldValue(R.id.create_user));
+		params.put("password", getTextFieldValue(R.id.create_pass));
+		params.put("email", getTextFieldValue(R.id.create_emailaddress));
+
+		Log.e(TAG, "Go create!");
+		getAsyncHttpClient().post(BASE_URL + "account/create", params, new AsyncHttpResponseHandler() {
+			@Override
+			public void onStart() {
+				Log.i(TAG, "Started request!");
+			}
+
+			@Override
+			public void onSuccess(String response) {
+				Log.i(TAG, "Got success response: " + response);
+			}
+
+			@Override
+			public void onFailure(Throwable e, String response) {
+				Log.i(TAG, "Got failure response: " + response + ". Throwable: " + e);
+			}
+
+			@Override
+			public void onFinish() {
+				Log.i(TAG, "Done with request!");
+			}
+		});
+	}
+
+	public void onLoginClick(View view) {
+		RequestParams params = new RequestParams();
+		params.put("username", getTextFieldValue(R.id.login_username));
+		params.put("password", getTextFieldValue(R.id.login_password));
+
+		Log.e(TAG, "Go login!");
+		getAsyncHttpClient().post(BASE_URL + "account/login", params, new AsyncHttpResponseHandler() {
+			@Override
+			public void onStart() {
+				Log.i(TAG, "Started request!");
+			}
+
+			@Override
+			public void onSuccess(String response) {
+				Log.i(TAG, "Got success response: " + response);
+			}
+
+			@Override
+			public void onFailure(Throwable e, String response) {
+				Log.i(TAG, "Got failure response: " + response + ". Throwable: " + e);
+			}
+
+			@Override
+			public void onFinish() {
+				Log.i(TAG, "Done with request!");
+			}
+		});
+	}
+
+	public void onAuthTestClick(View view) {
+		Log.e(TAG, "Go testauth!");
+		getAsyncHttpClient().get(BASE_URL + "testauth", new RequestParams(), new AsyncHttpResponseHandler() {
+			@Override
+			public void onStart() {
+				Log.i(TAG, "Started request!");
+			}
+
+			@Override
+			public void onSuccess(String response) {
+				Log.i(TAG, "Got success response: " + response);
+			}
+
+			@Override
+			public void onFailure(Throwable e, String response) {
+				Log.i(TAG, "Got failure response: " + response + ". Throwable: " + e);
+			}
+
+			@Override
+			public void onFinish() {
+				Log.i(TAG, "Done with request!");
+			}
+		});
+	}
+
+	public void onAnotherClick(View view) {
+		startActivity(new Intent(this, SecondaryTestActivity.class));
+	}
 }
---
+++ TestHttpRequests/src/com/example/testhttprequests/SecondaryTestActivity.java
@@ -0,0 +1,70 @@
+package com.example.testhttprequests;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.PersistentCookieStore;
+import com.loopj.android.http.RequestParams;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+
+public class SecondaryTestActivity extends Activity {
+	private static final String TAG = "SecondaryTestActivity";
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_secondary_test);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.secondary_test, menu);
+		return true;
+	}
+	
+	private AsyncHttpClient asyncClient = null;
+	private static final String BASE_URL = "http://10.0.2.2:5000/v1/"; // localhost!
+	private AsyncHttpClient getAsyncHttpClient() {
+		if (asyncClient == null) {
+			asyncClient = new AsyncHttpClient();
+			asyncClient.setCookieStore(new PersistentCookieStore(getApplication()));
+		}
+		return asyncClient;
+	}
+	
+	public void onAuthTestClick(View view) {
+		Log.e(TAG, "Go testauth!");
+		getAsyncHttpClient().get(BASE_URL + "testauth", new RequestParams(), new AsyncHttpResponseHandler() {
+		     @Override
+		     public void onStart() {
+		    	 Log.i(TAG, "Started request!!");
+		     }
+
+		     @Override
+		     public void onSuccess(String response) {
+		    	 Log.i(TAG, "Got success response: " + response);
+		     }
+		 
+		     @Override
+		     public void onFailure(Throwable e, String response) {
+		    	 Log.i(TAG, "Got failure response: " + response + ". Throwable: " + e);
+		     }
+
+		     @Override
+		     public void onFinish() {
+		    	 Log.i(TAG, "Done with request!");
+		     }
+		});
+	}
+	
+	public void onGoBackClick(View view) {
+		startActivity(new Intent(this, MainActivity.class));
+	}
+}
