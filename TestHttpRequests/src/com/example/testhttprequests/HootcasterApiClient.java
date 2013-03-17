5ccfe3cacbea081a8f73387c033e3159e24dccc6
Matt Spitz <mattspitz@gmail.com>
2013-03-17 14:25:14 -0400

    Correctly accept all SSL certificates!

--- TestHttpRequests/src/com/example/testhttprequests/HootcasterApiClient.java
+++ TestHttpRequests/src/com/example/testhttprequests/HootcasterApiClient.java
@@ -1,9 +1,24 @@
 package com.example.testhttprequests;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.ConnectException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import org.apache.http.HttpEntity;
+import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.apache.http.entity.StringEntity;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -14,6 +29,7 @@ import android.util.Log;
 import com.google.common.base.Preconditions;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.PersistentCookieStore;
 
 public class HootcasterApiClient {
@@ -29,6 +45,40 @@ public class HootcasterApiClient {
 		this.context = context;
 		this.asyncHttpClient = new AsyncHttpClient();
 		this.asyncHttpClient.setCookieStore(new PersistentCookieStore(this.context));
+
+		if (isDevEnvironment()) {
+			// accept certificates from all hostnames
+			KeyStore trustStore;
+			try {
+				trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+			} catch (KeyStoreException ex) {
+				throw new RuntimeException(ex);
+			}
+			try {
+				trustStore.load(null, null);
+			} catch (NoSuchAlgorithmException ex) {
+				throw new RuntimeException(ex);
+			} catch (CertificateException ex) {
+				throw new RuntimeException(ex);
+			} catch (IOException ex) {
+				throw new RuntimeException(ex);
+			}
+
+			SSLSocketFactory socketFactory;
+			try {
+				socketFactory = new AcceptAllSSLSocketFactory(trustStore);
+			} catch (KeyManagementException ex) {
+				throw new RuntimeException(ex);
+			} catch (UnrecoverableKeyException ex) {
+				throw new RuntimeException(ex);
+			} catch (NoSuchAlgorithmException ex) {
+				throw new RuntimeException(ex);
+			} catch (KeyStoreException ex) {
+				throw new RuntimeException(ex);
+			}
+			socketFactory.setHostnameVerifier(SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+			this.asyncHttpClient.setSSLSocketFactory(socketFactory);
+		}
 	}
 
 	private static boolean IS_DEV_ENVIRONMENT = true; // TODO automatically determine this
@@ -46,7 +96,7 @@ public class HootcasterApiClient {
 		} catch (UnsupportedEncodingException ex) {
 			throw new RuntimeException(ex); // shouldn't ever happen
 		}
-		
+
 		Log.i(TAG, "URL: " + url);
 		Log.i(TAG, "Data: " + json.toString());
 		asyncHttpClient.post(
@@ -66,6 +116,7 @@ public class HootcasterApiClient {
 			final String username, final String password,
 			final String fullname, final String registrationId,
 			final String email, final String phone, final LoginHandler loginHandler) {
+
 		JSONObject json = new JSONObject();
 		try {
 			json.put("username", Preconditions.checkNotNull(username));
@@ -76,27 +127,28 @@ public class HootcasterApiClient {
 
 			if (phone != null)
 				json.put("phone", phone);
+
 		} catch (JSONException ex) {
 			throw new RuntimeException(ex); // shouldn't ever happen
 		}
 
-		jsonPost("account/create", true, json, new AsyncHttpResponseHandler() {
+		jsonPost("account/create", true, json, new JsonHttpResponseHandler() {
 			@Override
 			public void onStart() {
 				Log.i(TAG, "onStart");
 			}
 
 			@Override
-			public void onSuccess(final String response) {
-				Log.i(TAG, "Success!: " + response);
+			public void onSuccess(final JSONObject response) {
+				Log.i(TAG, "Success: " + response.toString());
 				loginHandler.handleSuccess();
 			}
 
 			@Override
-			public void onFailure(final Throwable ex, final String response) {
-				Log.i(TAG, "error: " + ex);
+			public void onFailure(final Throwable ex, final JSONObject response) {
+				Log.e(TAG, "Error: " + ex);
 				if (response != null)
-					Log.i(TAG, "content: " + response);
+					Log.e(TAG, "Content: " + response.toString());
 				if (ex instanceof ConnectException)
 					loginHandler.handleConnectionFailure();
 			}
@@ -106,5 +158,37 @@ public class HootcasterApiClient {
 				Log.i(TAG, "onFinish");
 			}
 		});
+	}
+
+	private static class AcceptAllSSLSocketFactory extends SSLSocketFactory {
+		SSLContext sslContext = SSLContext.getInstance("TLS");
+
+		public AcceptAllSSLSocketFactory(KeyStore truststore) throws NoSuchAlgorithmException, KeyManagementException, KeyStoreException, UnrecoverableKeyException {
+			super(truststore);
+
+			TrustManager tm = new X509TrustManager() {
+				public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+				}
+
+				public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+				}
+
+				public X509Certificate[] getAcceptedIssuers() {
+					return null;
+				}
+			};
+
+			sslContext.init(null, new TrustManager[] { tm }, null);
+		}
+
+		@Override
+		public Socket createSocket(Socket socket, String host, int port, boolean autoClose) throws IOException, UnknownHostException {
+			return sslContext.getSocketFactory().createSocket(socket, host, port, autoClose);
+		}
+
+		@Override
+		public Socket createSocket() throws IOException {
+			return sslContext.getSocketFactory().createSocket();
+		}
 	}
 }
